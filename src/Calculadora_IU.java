94c04430fc67b927ef9b1bd718397cf05af13243
yessie06 <yeseniamarley06@gmail.com>
2023-09-08 19:24:27 -0500

    agregue codigo para el boton multiplicar

--- src/Calculadora_IU.java
+++ src/Calculadora_IU.java
@@ -151,26 +151,26 @@ public class Calculadora_IU extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-         double nro1 = Double.parseDouble(txtNro1.getText());
+        double nro1 = Double.parseDouble(txtNro1.getText());
         double nro2 = Double.parseDouble(txtNro2.getText());
         double suma = nro1 - nro2;
-        JOptionPane.showMessageDialog(this, "la suma es: " + suma);
+        JOptionPane.showMessageDialog(this, "la resta es: " + suma);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
          double nro1 = Double.parseDouble(txtNro1.getText());
         double nro2 = Double.parseDouble(txtNro2.getText());
-        double suma = nro1 * nro2;
-        JOptionPane.showMessageDialog(this, "la suma es: " + suma);
+        double multiplicar = nro1 * nro2;
+        JOptionPane.showMessageDialog(this, "la mutiplicacion es: " + multiplicar);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-         double nro1 = Double.parseDouble(txtNro1.getText());
-        double nro2 = Double.parseDouble(txtNro2.getText());
-        double suma = nro1 / nro2;
-        JOptionPane.showMessageDialog(this, "la suma es: " + suma);
+//         double nro1 = Double.parseDouble(txtNro1.getText());
+//        double nro2 = Double.parseDouble(txtNro2.getText());
+//        double suma = nro1 / nro2;
+//        JOptionPane.showMessageDialog(this, "la suma es: " + suma);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
